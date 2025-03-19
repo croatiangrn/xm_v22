9829e2e36304c32fb9677ea2d1ff96cb16916091
Marko Mikulić <mikulic.marko996@gmail.com>
2025-03-19 13:29:29 +0100

    Added missing indexes and custom enum type for company type

--- internal/domain/company/entity.go
+++ internal/domain/company/entity.go
@@ -1,10 +1,19 @@
 package company
 
+type companyType string
+
+const (
+	CompanyTypeCorporations       companyType = "corporations"
+	CompanyTypeNonProfit          companyType = "non_profit"
+	CompanyTypeCooperative        companyType = "cooperative"
+	CompanyTypeSoleProprietorship companyType = "sole_proprietorship"
+)
+
 type Company struct {
-	ID                string `json:"id" gorm:"column:id;uuid;primaryKey;"`
-	Name              string `json:"name" gorm:"column:id;varchar(15);unique;not null"`
-	Description       string `json:"description" gorm:"column:description;varchar(3000)"`
-	AmountOfEmployees int    `json:"amount_of_employees" gorm:"column:amount_of_employees;int;not null"`
-	Registered        bool   `json:"registered" gorm:"column:registered;bool;not null"`
-	Type              string `json:"type" gorm:"column:type;varchar(255);not null"`
+	ID                string      `json:"id" gorm:"column:id;uuid;primaryKey;"`
+	Name              string      `json:"name" gorm:"column:id;varchar(15);uniqueIndex:uix_company_name;not null"`
+	Description       string      `json:"description" gorm:"column:description;varchar(3000)"`
+	AmountOfEmployees int         `json:"amount_of_employees" gorm:"column:amount_of_employees;int;not null"`
+	Registered        bool        `json:"registered" gorm:"column:registered;bool;not null"`
+	Type              companyType `json:"type" gorm:"column:type;type:company_type;varchar(255);not null"`
 }
--- internal/infrastructure/database/migrations.go
+++ internal/infrastructure/database/migrations.go
@@ -5,10 +5,40 @@ import (
 	"gorm.io/gorm"
 )
 
-func AutoMigrate(db *gorm.DB) error {
-	err := db.AutoMigrate(&company.Company{})
+func createEnumTypes(db *gorm.DB) error {
+	// Check if the enum type already exists
+	var exists bool
+	err := db.Raw(`
+        SELECT EXISTS (
+            SELECT 1 FROM pg_type WHERE typname = 'company_type'
+        );
+    `).Scan(&exists).Error
 	if err != nil {
 		return err
 	}
+
+	// If the enum type doesn't exist, create it
+	if !exists {
+		err = db.Exec(`
+            CREATE TYPE company_type AS ENUM (
+                'corporations', 'non_profit', 'cooperative', 'sole_proprietorship'
+            );
+        `).Error
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+func AutoMigrate(db *gorm.DB) error {
+	if err := createEnumTypes(db); err != nil {
+		return err
+	}
+
+	if err := db.AutoMigrate(&company.Company{}); err != nil {
+		return err
+	}
+
 	return nil
 }
