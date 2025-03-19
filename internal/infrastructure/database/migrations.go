3e018e42afcc36a5960ddb70d89e9ec0c773da81
Marko Mikulić <mikulic.marko996@gmail.com>
2025-03-19 22:51:54 +0100

    Close conn after RunMigrations is done

--- internal/infrastructure/database/migrations.go
+++ internal/infrastructure/database/migrations.go
@@ -6,6 +6,7 @@ import (
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/pgx"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"log"
 )
 
 // RunMigrations applies database migrations using SQL files.
@@ -16,6 +17,12 @@ func RunMigrations(dsn, migrationsPath string) error {
 	if err != nil {
 		return fmt.Errorf("failed to open database connection: %w", err)
 	}
+
+	defer func() {
+		if err := d.Close(); err != nil {
+			log.Printf("Failed to close database connection: %v", err)
+		}
+	}()
 
 	m, err := migrate.NewWithDatabaseInstance(migrationsPath, "pgx", d)
 	if err != nil {
