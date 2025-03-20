4c94078244b73f45e735895ddeb358038b29b7d5
Marko Mikulić <mikulic.marko996@gmail.com>
2025-03-20 10:07:13 +0100

    Removed validations from DTOs
    Added timestamps to companies table

--- internal/controller/http/dto/company.go
+++ internal/controller/http/dto/company.go
@@ -1,17 +1,17 @@
 package dto
 
 type CreateCompanyRequest struct {
-	Name              string `json:"name" binding:"required"`
-	Description       string `json:"description" binding:"omitempty"`
-	AmountOfEmployees int    `json:"amount_of_employees" binding:"required,gte=0"`
-	Registered        bool   `json:"registered" binding:"omitempty"`
-	Type              string `json:"type" binding:"required,oneof=NonProfit Corporations Cooperative 'Sole Proprietorship'"`
+	Name              string `json:"name"`
+	Description       string `json:"description"`
+	AmountOfEmployees int    `json:"amount_of_employees"`
+	Registered        bool   `json:"registered"`
+	Type              string `json:"type"`
 }
 
 type UpdateCompanyRequest struct {
-	Name              string `json:"name" binding:"required"`
-	Description       string `json:"description" binding:"omitempty"`
-	AmountOfEmployees int    `json:"amount_of_employees" binding:"required,gte=0"`
-	Registered        bool   `json:"registered" binding:"omitempty"`
-	Type              string `json:"type" binding:"required,oneof=NonProfit Corporations Cooperative 'Sole Proprietorship'"`
+	Name              string `json:"name"`
+	Description       string `json:"description"`
+	AmountOfEmployees int    `json:"amount_of_employees"`
+	Registered        bool   `json:"registered"`
+	Type              string `json:"type"`
 }
